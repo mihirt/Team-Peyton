6654684d5f5c7039345955023c17cd1adeab6625
Ria Sonecha <ria@Vatsals-MBP-2.hsd1.ca.comcast.net>
2020-08-08 15:45:15 -0700

    switch to mp3

--- play_audio.py
+++ play_audio.py
@@ -3,22 +3,22 @@ import os
 audio_path = "audio_files"
 
 def say_play():
-    playsound(audio_path+"/Play.wav")
+    playsound(audio_path+"/Play.mp3")
 
 def say_pause():
-    playsound(audio_path+"/Pause.wav")
+    playsound(audio_path+"/Pause.mp3")
 
 def say_skip():
-    playsound(audio_path+"/Skip.wav")
+    playsound(audio_path+"/Skip.mp3")
 
 def say_help():
-    playsound(audio_path+"/Help.wav")
+    playsound(audio_path+"/Help.mp3")
 
 def say_hungry():
-    playsound(audio_path+"/Hungry.wav")
+    playsound(audio_path+"/Hungry.mp3")
 
 def say_thirsty():
-    playsound(audio_path+"/Thirsty.wav")
+    playsound(audio_path+"/Thirsty.mp3")
 
 
 say_play()
