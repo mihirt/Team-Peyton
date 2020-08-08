a3715db8fea0d68e178c04d9dff0eac1504c4ad0
Ria Sonecha <ria@Vatsals-MBP-2.hsd1.ca.comcast.net>
2020-08-08 15:06:18 -0700

    fixing play audio

--- play_audio.py
+++ play_audio.py
@@ -1,13 +1,4 @@
-# Sound2a.py
+from playsound import playsound
 
-from soundplayer import SoundPlayer
-import time
-
-# Sine tone during 0.1 s, blocking, device 0
-dev = 0
-SoundPlayer.playTone(900, 0.1, True, dev) # 900 Hz
-SoundPlayer.playTone(800, 0.1, True, dev) # 600 Hz
-SoundPlayer.playTone(600, 0.1, True, dev) # 600 Hz
-time.sleep(1)
-SoundPlayer.playTone([900, 800, 600], 5, True, dev) # 3 tones together
-print "done"
+playsound("sound_samples/drum_cymbal_open.wav")
+print("done")
--- test_pygame.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-pygame.init()
-drum = pygame.mixer.Sound("/home/pi/Team-Peyton/gpio-music-box/samples/drum_tom_mid_hard.wav")
-drum.play()
-print("done")
