724dc3c0c760e7b13ed61547ce8572de2c75782a
Mihir Trivedi <mihirt@mit.edu>
2020-08-08 16:56:11 -0700

    add channel

--- play_audio.py
+++ play_audio.py
@@ -1,29 +1,36 @@
 from omxplayer.player import OMXPlayer
 audio_path = "audio_files"
 
+
 def say_play():
-    player = OMXPlayer(audio_path+"/Play.mp3")
+    player = OMXPlayer(audio_path + "/Play.mp3")
     player.play()
+
 
 def say_pause():
-    player = OMXPlayer(audio_path+"/Pause.mp3")
+    player = OMXPlayer(audio_path + "/Pause.mp3")
     player.play()
+
 
 def say_skip():
-    player = OMXPlayer(audio_path+"/Skip.mp3")
+    player = OMXPlayer(audio_path + "/Skip.mp3")
     player.play()
 
-def say_help():
-    player = OMXPlayer(audio_path+"/Help.mp3")
+
+def say_help(channel):
+    player = OMXPlayer(audio_path + "/Help.mp3")
     player.play()
+
 
 def say_hungry():
-    player = OMXPlayer(audio_path+"/Hungry.mp3")
+    player = OMXPlayer(audio_path + "/Hungry.mp3")
     player.play()
 
+
 def say_thirsty():
-    player = OMXPlayer(audio_path+"/Thirsty.mp3")
+    player = OMXPlayer(audio_path + "/Thirsty.mp3")
     player.play()
+
 
 if __name__ == '__main__':
     say_play()
