44617f73bc1ab5f684b2ccffce1918716908bbde
Mihir Trivedi <mihirtrivedi@C02DF9Z2MD6M.tld>
2020-10-18 21:16:12 -0400

    new pyaudio and pull script

---
+++ gitpull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/Team-Peyton/
+git pull origin master
+
+# @reboot ~/gitpull.sh
---
+++ play_audio_pygame.py
@@ -0,0 +1,43 @@
+import pygame
+audio_path = "audio_files"
+pygame.init()
+pygame.mixer.init()
+
+def say_play(channel=None):
+    file = audio_path + "/Play.mp3"
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+def say_pause(channel=None):
+    file = audio_path + "/Pause.mp3"
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+def say_skip(channel=None):
+    file = audio_path + "/Skip.mp3"
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+def say_help(channel=None):
+    file = audio_path + "/Help.mp3"
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+def say_hungry(channel=None):
+    file = audio_path + "/Hungry.mp3"
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+def say_thirsty(channel=None):
+    file = audio_path + "/Thirsty.mp3"
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+if __name__ == '__main__':
+    say_play()
