557a6aa922801ccccc1806065d480890bed8cc6a
Mihir Trivedi <mihirtrivedi@C02DF9Z2MD6M.tld>
2020-10-18 22:22:25 -0400

    ticking

--- play_audio_pygame.py
+++ play_audio_pygame.py
@@ -7,37 +7,49 @@ def say_play(channel=None):
     file = audio_path + "/Play.mp3"
     pygame.mixer.music.load(file)
     pygame.mixer.music.play()
-    pygame.event.wait()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(3)
+
 
 def say_pause(channel=None):
     file = audio_path + "/Pause.mp3"
     pygame.mixer.music.load(file)
     pygame.mixer.music.play()
-    pygame.event.wait()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(3)
+
 
 def say_skip(channel=None):
     file = audio_path + "/Skip.mp3"
     pygame.mixer.music.load(file)
     pygame.mixer.music.play()
-    pygame.event.wait()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(3)
+
 
 def say_help(channel=None):
     file = audio_path + "/Help.mp3"
     pygame.mixer.music.load(file)
     pygame.mixer.music.play()
-    pygame.event.wait()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(3)
+
 
 def say_hungry(channel=None):
     file = audio_path + "/Hungry.mp3"
     pygame.mixer.music.load(file)
     pygame.mixer.music.play()
-    pygame.event.wait()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(3)
+
 
 def say_thirsty(channel=None):
     file = audio_path + "/Thirsty.mp3"
     pygame.mixer.music.load(file)
     pygame.mixer.music.play()
-    pygame.event.wait()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(3)
+
 
 if __name__ == '__main__':
     say_play()
