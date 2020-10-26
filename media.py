6c1e70608e953ec985d37bf894043b92f7c78e0d
Mihir Trivedi <mihirtrivedi@C02DF9Z2MD6M.tld>
2020-10-25 23:44:24 -0400

    update media.py

--- media.py
+++ media.py
@@ -151,5 +151,5 @@ if __name__ == '__main__':
     # glib_thread = threading.Thread(target=GLib.MainLoop().run())
     # glib_thread.daemon = True
     # glib_thread.start()
-    print("suppity")
+    print("downloaded")
     signal.pause()
